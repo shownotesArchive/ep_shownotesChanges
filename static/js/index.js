5b2d5ced1ece10d7bd76bc6840df6abd9f279467
mluto <m@luto.at>
2013-03-28 19:30:14 +0100

    Hide buttons via CSS, linenumbers white background

--- static/js/index.js
+++ static/js/index.js
@@ -3,4 +3,5 @@ exports.postAceInit = function(hook, context)
   $("#settingslink").remove();
   $("#usericon").prev().remove(); // seperator
   $("#embedlink").remove();
+  $("iframe[name=ace_outer]").contents().find("#sidediv").css('background-color', 'white');
 }
