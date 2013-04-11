ad434b2f252d3ba87ed25213206fdefd50d882fb
mluto <m@luto.at>
2013-04-11 08:41:33 +0200

    Fix height of embedded pad

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ exports.eejsBlock_styles = function (hook, context)
   var resizeChat = '<style type="text/css">\n';
   resizeChat += '#editbar {border-bottom: none;background: white;}';
   resizeChat += '.menu_left, #settingslink, #embedlink {display:none;}';
-  resizeChat += 'iframe[name=ace_outer] {top:-32px;}';
+  resizeChat += 'iframe[name=ace_outer] {top:-32px; height: calc(100% + 32px) !important}';
   resizeChat += '</style>\n';
   
   context.content = context.content + resizeChat;
